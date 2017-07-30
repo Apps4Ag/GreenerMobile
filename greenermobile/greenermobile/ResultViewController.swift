98ff71cfcf7e457683ec5fb7fe4612ebdcd6f572
Scott Kirkland <srkirkland@gmail.com>
2017-07-30 10:13:53 -0700

    update descriptions

--- greenermobile/greenermobile/ResultViewController.swift
+++ greenermobile/greenermobile/ResultViewController.swift
@@ -31,15 +31,23 @@ class ResultViewController: UIViewController {
         lblDiagnosis.text = result.diagnosis
         lblPlantName.text = result.plantName
         lblLatinName.text = result.latinName
-        lblDate.text = "29 July 2017"
+        lblDate.text = "30 July 2017"
         lblDateTaken.text = "Date Taken"
+        txtDescription.text = result.description
         
         if (lblDiagnosis.text == "gardenia bad") {
             lblDiagnosis.text = "Mineral Deficiency"
+            lblPlantName.text = "Gardenia"
+            lblLatinName.text = "Gardenia jasminoides"
+            txtDescription.text = "Certain nutrients, in relatively small amounts, are required for healthy plant growth. Deficiencies can cause tip chlorosis or necrosis or cause foliage to discolor, fade, distort, or become spotted, sometimes in a characteristic pattern that can be recognized to identify the cause. Fewer leaves, flowers, and fruit may be produced, and these can develop later than normal and remain undersized. More severely deficient plants become stunted and exhibit dieback. Nutrient deficiencies rarely occur in most woody landscape plants. When they do occur, nitrogen and iron are the most commonly seen deficiencies. However, nutrient deficiency symptoms usually are not due to a deficiency in the soil but are caused by conditions that reduce a plant's access to them. Poor root growth caused by water-logged soil, root diseases, and nematodes can cause deficiency symptoms. Commercial laboratories can conduct foliage tests or soil analysis to verify deficiencies."
         } else if (lblDiagnosis.text == "gardenia good") {
             lblDiagnosis.text = "No Issues Detected"
+            lblPlantName.text = "Gardenia"
+            lblLatinName.text = "Gardenia jasminoides"
+            txtDescription.text = "Gardenias are known for waxy white fragrant blooms upon large dark green leaves. Plants are grown as evergreen shrubs, groundcovers or as a formal standard. They can also be espaliered. They can grow up to 6 feet tall. \n Gardenias are best grown in rich organic soils with acidic pH. They require well-drained soil. Avoid crowding with other plants and use surface cultivation. Plants do best in partial shade in hot inland valleys. Avoid high salt conditions. Mulches may help conserve water and decrease soil temperature fluctuations."
         }
     }
+    @IBOutlet weak var txtDescription: UITextView!
 
     @IBOutlet weak var imgResultPhoto: UIImageView!
     
--- greenermobile/greenermobile/ViewController.swift
+++ greenermobile/greenermobile/ViewController.swift
@@ -11,16 +11,17 @@ import UIKit
 class ResultDetail {
     init() {}
 
-    init(diagnosis: String, plantName: String, latinName: String) {
+    init(diagnosis: String, plantName: String, latinName: String, description: String) {
         self.diagnosis = diagnosis
         self.plantName = plantName
         self.latinName = latinName
-        ViewController.plantImage = UIImage(named: "strawberry")
+        self.description = description
     }
 
     var diagnosis: String = "Root Rot"
     var plantName: String = "Tomato"
     var latinName: String = "Tomatus Plantus"
+    var description: String = "Lorem ipsum"
 }
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -74,15 +75,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         switch sender.tag {
         case 0:
-            ViewController.plantResult = ResultDetail(diagnosis: "Asian Citrus Psyllid", plantName: "Citrus", latinName: "Citreae")
+            ViewController.plantResult = ResultDetail(diagnosis: "Asian Citrus Psyllid", plantName: "Citrus", latinName: "Citreae", description: "The Asian citrus psyllid, Diaphorina citri (or ACP), is a tiny, mottled brown insect about the size of an aphid that poses a serious threat to California's citrus trees—including those grown in home gardens and on farms. The psyllid feeds on all varieties of citrus (e.g., oranges, grapefruit, lemons, and mandarins) and several closely related ornamental plants in the family Rutaceae (e.g., calamondin, box orange, Indian curry leaf, and orange jessamine or orange jasmine).")
             ViewController.plantImage = UIImage(named: "acp")
         case 1:
-            ViewController.plantResult = ResultDetail(diagnosis: "Botrytis Fruit Rot", plantName: "Strawberry", latinName: "Fragaria × ananassa")
+            ViewController.plantResult = ResultDetail(diagnosis: "Botrytis Fruit Rot", plantName: "Strawberry", latinName: "Fragaria × ananassa", description: "The fungus that causes Botrytis fruit rot, also known as gray mold, is widespread in the environment. It can infect strawberry flowers when spores landing on them are exposed to free water and cool temperatures. Infections can either cause flowers to rot or Botrytis can become dormant in floral tissues. Dormant infections resume activity on the berry later in the season anytime before or after harvest when sugars increase and conditions become favorable to disease development")
             ViewController.plantImage = UIImage(named: "strawberry")
         case 2:
-            ViewController.plantResult = ResultDetail(diagnosis: "Powdery Mildew", plantName: "Strawberry", latinName: "Fragaria × ananassa")
+            ViewController.plantResult = ResultDetail(diagnosis: "Powdery Mildew", plantName: "Strawberry", latinName: "Fragaria × ananassa", description: "Leaves infected with powdery mildew initially have small, white powdery colonies on the undersides of leaves. These colonies enlarge to cover the entire lower leaf surface, causing the edges of the leaves to roll up. Purple reddish blotches appear on the upper and lower surface of leaves. Infected flowers produce deformed fruit or no fruit at all. Severely infected flowers may be completely covered by mycelium and killed. Infected immature fruits become hardened and desiccated. Infected mature fruits become seedy in appearance and support spore-producing colonies that look powdery and white.")
         case 3:
-            ViewController.plantResult = ResultDetail(diagnosis: "Rot something", plantName: "Strawberry", latinName: "Stawbericus Maximus")
+            ViewController.plantResult = ResultDetail(diagnosis: "Rot something", plantName: "Strawberry", latinName: "Stawbericus Maximus", description: "Leaves infected with powdery mildew initially have small, white powdery colonies on the undersides of leaves. These colonies enlarge to cover the entire lower leaf surface, causing the edges of the leaves to roll up. Purple reddish blotches appear on the upper and lower surface of leaves. Infected flowers produce deformed fruit or no fruit at all. Severely infected flowers may be completely covered by mycelium and killed. Infected immature fruits become hardened and desiccated. Infected mature fruits become seedy in appearance and support spore-producing colonies that look powdery and white.")
         default:
             ViewController.plantResult = ResultDetail()
         }
